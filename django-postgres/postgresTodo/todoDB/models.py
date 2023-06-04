8fd574ac71cabf554bfcbff9238918bfe805c2df
Adrian <jose_a_vazquez@outlook.com>
2023-06-04 15:09:48 -0600

    Bug in was_published_recently()

--- django-postgres/postgresTodo/todoDB/models.py
+++ django-postgres/postgresTodo/todoDB/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
         
     # Si ha sido publicado en las últimas 24 horas
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     class Meta:
